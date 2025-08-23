88544987566022b5831a0a8634b96aba51267d06
ychleo102615 <leo102615@gmail.com>
2025-08-23 14:49:15 +0800

    加入ghpage

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vite-rookie-ms1/',
   plugins: [
     vue(),
     vueDevTools(),
